0b15d66a1cfd675f8984a5784e95a2ee82b65a5f
Yusron H <yusronzain@gmail.com>
2022-04-26 18:21:13 +0700

    Initialize Riverpod

--- lib/main.dart
+++ lib/main.dart
@@ -1,25 +1,47 @@
 import 'package:e_commerce10/firebase_options.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(const ProviderScope(child: MyApp()));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({ Key? key }) : super(key: key);
+final counterProvider = StateNotifierProvider((ref) {
+  return Counter();
+});
+
+class Counter extends StateNotifier<int> {
+  Counter() : super(0);
+
+  void increment() => state++;
+}
+
+class MyApp extends ConsumerWidget {
+  const MyApp({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
+    final count = ref.watch(counterProvider);
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const Scaffold(),
+      home: Scaffold(
+        body: Center(
+          child: Text(count.toString()),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            ref.read(counterProvider.notifier).increment();
+          },
+          child: const Icon(Icons.add),
+        ),
+      ),
     );
   }
 }
