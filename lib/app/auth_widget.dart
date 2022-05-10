e7343399c321cef1ecf8b98dfccb96f263dd33c8
Yusron H <yusronzain@gmail.com>
2022-05-10 23:04:06 +0700

    Add authenticated users stored to firestore database

--- lib/app/auth_widget.dart
+++ lib/app/auth_widget.dart
@@ -1,4 +1,5 @@
 import 'package:e_commerce10/app/providers.dart';
+import 'package:e_commerce10/models/user_data.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -23,9 +24,7 @@ class AuthWidget extends ConsumerWidget {
 
     return authStateChanges.when(
       data: (user) => user != null
-          ? user.email == adminEmail
-              ? adminSignedInBuilder(context)
-              : signedInBuilder(context)
+          ? signedInHandler(context, ref, user)
           : nonSignedInBuilder(context),
       loading: () => const Scaffold(
         body: Center(
@@ -37,5 +36,32 @@ class AuthWidget extends ConsumerWidget {
         child: Text("Something went wrong!"),
       )),
     );
+  }
+
+  FutureBuilder<UserData?> signedInHandler(context, WidgetRef ref, User user) {
+    final database = ref.read(databaseProvider)!;
+    final potentialUserFuture = database.getUser(user.uid);
+    return FutureBuilder<UserData?>(
+        future: potentialUserFuture,
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.done) {
+            final potentialUser = snapshot.data;
+            if (potentialUser == null) {
+              database.addUser(UserData(
+                  email: user.email != null ? user.email! : "",
+                  uid: user
+                      .uid)); // no need to await as you don't depend on that
+            }
+            if (user.email == adminEmail) {
+              return adminSignedInBuilder(context);
+            }
+            return signedInBuilder(context);
+          }
+          return const Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        });
   }
 }
--- lib/services/firestore_service.dart
+++ lib/services/firestore_service.dart
@@ -1,5 +1,6 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:e_commerce10/models/product.dart';
+import 'package:e_commerce10/models/user_data.dart';
 
 class FirestoreService {
   FirestoreService({required this.uid});
@@ -27,5 +28,16 @@ class FirestoreService {
 
   Future<void> deleteProduct(String id) async {
     return await firestore.collection("products").doc(id).delete();
+  }
+
+  Future<void> addUser(
+    UserData user,
+  ) async {
+    await firestore.collection("users").doc(user.uid).set(user.toMap());
+  }
+
+  Future<UserData?> getUser(String uid) async {
+    final doc = await firestore.collection("users").doc(uid).get();
+    return doc.exists ? UserData.fromMap(doc.data()!) : null;
   }
 }
